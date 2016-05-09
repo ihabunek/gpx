8b9afc529f04b2f7f0746b7934ddde3615015abf
Ivan Habunek <ivan.habunek@gmail.com>
2016-05-09 07:56:54 +0200

    Fix description in project file

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject gpx "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Parser and visualizer for gpx tracks"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT Lincense"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [
     [org.clojure/clojure  "1.8.0"]
     [org.clojure/data.zip "0.1.1"]
